fa0d4064aa8bd633b182daed331ec121716446d8
kaicmurilo <kaicmurilo@live.com>
2018-09-12 14:28:12 -0400

    CRUD

--- bin/server.js
+++ bin/server.js
@@ -1,25 +1,11 @@
-const app = require('../src/app');
-const debug = require('debug')('nodestr:server'); //IMPORTANDO DEBUG
+const app = require('../src/app'); // IMPORTANDO APP.JS 
+const debug = require('debug')('balta:server'); //IMPORTANDO DEBUG
 const http = require('http'); // IMPORTANDO HTTP
 
-//IMPORTANDO EXPRESS
-const express = require('express');
-
-const app = express();
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 const server = http.createServer(app);
-const router = express.Router();
-
-const route = router.get('/', (req,res,next) =>{
-    res.status(200).send({
-        title: "Node Store API",
-        version: "0.0.1"
-    });
-});
-
-app.use('/',route);
 
 server.listen(port);
 server.on('error', onError);
@@ -27,8 +13,8 @@ server.on('listening', onListening);
 console.log('Api rodando na porta ' +port);
 
 //gerador de código do express
-function normalizePort(val){
-    const port = parseInt(val,10);
+ function normalizePort(val){
+    const port = parseInt(val, 10);
 
     if(isNaN(port)){
         return val;
--- src/app.js
+++ src/app.js
@@ -1,14 +1,35 @@
 const express = require('express');//IMPORTANDO EXPRESS
+const bodyParser = require('body-parser');
 
 const app = express();
 const router = express.Router();
 
-const route = router.get('/', (req,res,next) =>{
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const route = router.get('/', (req, res, next) => {
     res.status(200).send({
-         title: "Node Store API",
-         version: "0.0.1"
+        title: "Node Store API",
+        version: "0.0.1"
     });
- });
-app.use('/',route);
+});
+
+const create = router.post('/', (req, res, next) => {
+    res.status(201).send(req.body);
+});
+
+const put = router.put('/:id', (req, res, next) => {
+    const id = req.params.id;
+    res.status(201).send({ id: id, item: req.body });
+});
+
+const del = router.delete('/', (req, res, next) => {
+    res.status(200).send(req.body);
+});
+
+app.use('/', route);
+app.use('/products', create);
+app.use('/products', put);
+app.use('/products', del);
 
 module.exports = app; //EXPORTANDO 
